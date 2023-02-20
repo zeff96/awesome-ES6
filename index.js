b440afea068574b18be67e9a0a148526a9b6a0ad
zeff96 <zeffo1975@gmail.com>
2023-02-20 22:32:40 +0300

    Add luxon time formating from npm package, add user alert message when the book has been added succesfully.

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import { render } from './modules/app.js';
 import addBooks, { addTitle, addAuthor } from './modules/create.js';
 import toggleMenu from './modules/menu.js';
-import updateTime from './modules/time.js';
+import { DateTime } from './node_modules/luxon/src/luxon.js';
 
 const addBtn = document.querySelector('.btn');
+const dateContainer = document.querySelector('.date-container');
+const alertBox = document.querySelector('.alert');
+
+const addZero = (num) => ((num < 10) ? `0${num}` : num);
+
+const updateTime = () => {
+  const date = DateTime.local().toLocaleString(DateTime.DATE_MED);
+  const { hour } = DateTime.local().c;
+  const min = DateTime.local().c.minute;
+  const sec = addZero(DateTime.local().c.second);
+
+  dateContainer.innerHTML = `${date} ${hour}:${min}:${sec}`;
+};
 
 setInterval(updateTime, 1000);
 
@@ -12,6 +25,17 @@ addBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
   addBooks();
+  const showAlert = () => {
+    alertBox.classList.add('active');
+  };
+  showAlert();
+
+  const removeAlert = () => {
+    alertBox.classList.remove('active');
+  };
+
+  setTimeout(removeAlert, 2000);
+
   addTitle.value = '';
   addAuthor.value = '';
 });
